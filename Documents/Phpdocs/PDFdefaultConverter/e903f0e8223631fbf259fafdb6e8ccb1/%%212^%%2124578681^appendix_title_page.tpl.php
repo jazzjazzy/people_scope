dd8e0855c965e812a8ef232aa497baebf43c4dd5
jason <jason@Silverberg>
2011-01-13 21:52:32 +1100

    added

--- Documents/Phpdocs/PDFdefaultConverter/e903f0e8223631fbf259fafdb6e8ccb1/%%212^%%2124578681^appendix_title_page.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.0, created on 2011-01-13 21:45:10
-         compiled from appendix_title_page.tpl */ ?>
-<newpage />
-<text size="26" justification="centre"><C:rf:1Appendices><b>Appendices</b>
-</text>
-<newpage />
