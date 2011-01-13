c6f2e4730cd9eba470ac56f9cbb12bc7059e58cb
jason <jason@Silverberg>
2011-01-13 21:51:26 +1100

    new Phpdocuments

---
+++ Documents/Phpdocs/PDFdefaultConverter/e903f0e8223631fbf259fafdb6e8ccb1/%%138^%%1381784439^source_title_page.tpl.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.0, created on 2011-01-13 21:45:10
+         compiled from source_title_page.tpl */ ?>
+<newpage />
+<text size="26" justification="centre"><C:rf:2Appendix C - Source Code>Appendix C - Source Code
+</text>
+<newpage />
---
+++ Documents/Phpdocs/PDFdefaultConverter/e903f0e8223631fbf259fafdb6e8ccb1/%%179^%%1794143582^pagenumbering.tpl.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.0, created on 2011-01-13 21:45:11
+         compiled from pagenumbering.tpl */ ?>
+<pdffunction:ezStartPageNumbers x="500" y="28" size="10" pos="" pattern="Page {PAGENUM} of {TOTALPAGENUM}" num="1" />
