862fc73c66b8af367b646d6f7b1af0e5d88cef05
jason <jason@Silverberg>
2011-01-13 21:59:26 +1100

    Added new phpunit tests

---
+++ test/AllTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once 'PHPUnit/Framework.php';
+require_once 'package/AllTests.php';
+
+class AllTests
+{
+	public static function suite()
+	{
+		$suite = new PHPUnit_Framework_TestSuite('Project');
+		$suite->addTest(Package_AllTests::suite());
+		
+		return $suite;
+	}
+}
+?>
